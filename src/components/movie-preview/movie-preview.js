3f3014747fd9fef17abd0a592e218d6d0c95e91a
Mihail <miha04635@gmail.com>
2024-03-26 16:46:02 +0300

    stub

--- src/components/movie-preview/movie-preview.js
+++ src/components/movie-preview/movie-preview.js
@@ -5,6 +5,14 @@ import './movie-preview.css'
 
 const MoviePreview = ({ posterPath }) => {
   const URL = 'https://image.tmdb.org/t/p/original'
+  if (!posterPath) {
+    return (
+      <div className="box">
+        <img alt="фото" className="stub" />
+      </div>
+    )
+  }
+
   return (
     <div className="box">
       <img className="image" src={URL + posterPath} alt="Preview" />
